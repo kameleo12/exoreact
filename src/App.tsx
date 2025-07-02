cb587e4e785d58de4205ff7bbe39d893e265b76b
Bodoule Sosso Leo <leobodoule@gmail.com>
2025-07-02 18:33:13 +0200

    exo react

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import ListGroup from "./components/ListGroup";
+
+function App() {
+  let items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
+  return (
+    <div>
+      <ListGroup items={items} heading="Cities"></ListGroup>
+    </div>
+  );
+}
+
+export default App;
